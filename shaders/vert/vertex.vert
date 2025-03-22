c5142fc439a5a85c3957de54fd99059ff7f74092
kennek4 <kentabanay@gmail.com>
2025-03-22 02:23:21 -0600

    feat: Now with snoopy, learned how to texture map

--- shaders/vert/vertex.vert
+++ shaders/vert/vertex.vert
@@ -1,8 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
 layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
+layout (location = 2) in vec2 aTexCoord; // the texture coord variable has attribute position 2 
   
 out vec3 ourColor; // output a color to the fragment shader
+out vec2 TexCoord;
 
 uniform float hOffSet;
 uniform float vOffSet;
@@ -14,5 +16,6 @@ void main()
     newPos.xy = vec2((aPos.x + hOffSet), (aPos.y + vOffSet));
     gl_Position = vec4(newPos, 1.0);
     ourColor = aColor; // set ourColor to the input color we got from the vertex data
+    TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }   
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,30 @@
 #include "shader.hpp"
 #include <GL/gl.h>
+#include <GL/glext.h>
 #include <GLES2/gl2.h>
 #include <GLES3/gl3.h>
 #include <GLFW/glfw3.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_error.h>
 #include <SDL3/SDL_events.h>
+#include <SDL3/SDL_filesystem.h>
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_iostream.h>
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_opengl.h>
 #include <SDL3/SDL_opengles2_gl2.h>
+#include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_scancode.h>
 #include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_surface.h>
 #include <SDL3/SDL_video.h>
+#include <SDL3_image/SDL_image.h>
 #include <cstddef>
 #include <glm/vec3.hpp>
+#include <iostream>
+#include <string>
 #include <utility>
 
 #define NUM_OF_SUBSYSTEMS 3
@@ -96,15 +104,22 @@ int main(int argc, char *argv[]) {
   // set up vertex data (and buffer(s)) and configure vertex attributes
   // ------------------------------------------------------------------
   float vertices[] = {
-      // Position                 Colours
-      -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // left
-      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // right
-      0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f  // top
+      // positions          // colors           // texture coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+  };
+  unsigned int indices[] = {
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
   };
 
-  unsigned int VBO, VAO;
+  unsigned int VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
   // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
   // then configure vertex attributes(s).
   glBindVertexArray(VAO);
@@ -112,25 +127,63 @@ int main(int argc, char *argv[]) {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
   // Vertex position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
   // The colour attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
                         (void *)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
 
-  // note that this is allowed, the call to glVertexAttribPointer registered VBO
-  // as the vertex attribute's bound vertex buffer object so afterwards we can
-  // safely unbind
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
 
-  // You can unbind the VAO afterwards so other VAO calls won't accidentally
-  // modify this VAO, but this rarely happens. Modifying other VAOs requires a
-  // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
-  // VBOs) when it's not directly necessary.
-  glBindVertexArray(0);
+  // Generating texture
+
+  // Generating texture
+  unsigned int texture;
+  glGenTextures(1, &texture);
+
+  // All GL_TEXTURE_2D operations will now use this texture
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  // Sets how the texture is wrapped for the S and T axis
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+  // Sets the texture sampling to be nearest when minifying a texture, and
+  // linear when upscaling (magnifying a texture)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // Get curr working dir
+  std::string texturePath = SDL_GetCurrentDirectory();
+  texturePath.append("src/textures/snoopy-esnupi.png");
+
+  /**
+   * If above path to the image file is valid
+   * we will use a SDL_Surface to "feed" the glTexImage2D function
+   * with the necessary pixels to create an OpenGL texture.
+   */
+  SDL_Surface *esnupi = IMG_Load(texturePath.c_str());
+  if (esnupi == NULL) {
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "FAILED TO LOAD IMAGE");
+    endProgram();
+  }
+
+  SDL_FlipSurface(esnupi, SDL_FLIP_VERTICAL);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, esnupi->w, esnupi->h, 0, GL_RGB,
+               GL_UNSIGNED_BYTE, esnupi->pixels);
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  SDL_DestroySurface(esnupi);
 
   float hOffSet = 0.0f;
   float vOffSet = 0.0f;
@@ -172,10 +225,11 @@ int main(int argc, char *argv[]) {
       glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
 
-      triangleShader.use();
+      glBindTexture(GL_TEXTURE_2D, texture);
 
+      triangleShader.use();
       glBindVertexArray(VAO);
-      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
       SDL_GL_SwapWindow(programWin);
     }
@@ -183,6 +237,7 @@ int main(int argc, char *argv[]) {
 
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
 
   endProgram();
 }
