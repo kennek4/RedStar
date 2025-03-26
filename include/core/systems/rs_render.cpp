f43c0d9fad3af6e2859d49dbfe272c5efa8356f1
kennek4 <kentabanay@gmail.com>
2025-03-25 21:44:56 -0600

    feat: Added RenderSystem OpenGL init logic

--- include/core/systems/rs_render.cpp
+++ include/core/systems/rs_render.cpp
@@ -2,6 +2,9 @@
 #include "rs_render.h"
 #include "rs_event_manager.h"
 #include "rs_events.h"
+#include <GL/gl.h>
+#include <GLES2/gl2.h>
+#include <GLES3/gl3.h>
 
 inline void ::RS::RenderSystem::emitEvent(const RS_EVENT event) {
   // Do nothing
@@ -13,4 +16,12 @@ inline void ::RS::RenderSystem::emitEvent(const RS_EVENT event) {
 
 inline void ::RS::RenderSystem::update(const RS_EVENT event) {
   _last_event = event;
+};
+
+void ::RS::RenderSystem::initOpenGL() {
+  glGenVertexArrays(1, &_vao);
+  glGenBuffers(1, &_vbo);
+  glGenBuffers(1, &_ebo);
+
+  glBindVertexArray(_vao);
 };
--- include/core/systems/rs_render.h
+++ include/core/systems/rs_render.h
@@ -4,6 +4,8 @@
 #include "rs_event_manager.h"
 #include "rs_events.h"
 #include "rs_system.h"
+#include <GLES2/gl2.h>
+#include <GLES3/gl3.h>
 #include <sys/types.h>
 
 namespace RS {
@@ -14,24 +16,35 @@ public:
     _event_manager = eventManager;
     _sid = sid;
     _last_event = RS_EVENT_NULL;
-  };
-
-  RenderSystem() {
-    _event_manager = NULL;
-    _last_event = RS_EVENT_NULL;
+    initOpenGL();
   };
 
   // Deconstructor
-  ~RenderSystem() { _event_manager = NULL; };
+  ~RenderSystem() {
+    _event_manager = NULL;
+
+    // TODO: Handle unintialiized vaos, vbos, or ebos
+    glDeleteVertexArrays(1, &_vao);
+    glDeleteBuffers(1, &_vbo);
+    glDeleteBuffers(1, &_ebo);
+  };
 
   void emitEvent(const RS_EVENT event) override;
   void update(const RS_EVENT event) override;
+
+  // Start all necessary OpenGL processes
+  void initOpenGL();
 
 private:
   RS_EVENT _last_event;
   EventManager *_event_manager;
   u_int _sid;
   bool isInitialized;
+
+  // OpenGL
+  u_int _vbo;
+  u_int _vao;
+  u_int _ebo;
 };
 } // namespace RS
 
