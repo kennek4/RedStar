4b2bc92a931063316afccd20c60aaa428d6cb362
kennek4 <kentabanay@gmail.com>
2025-03-29 14:26:29 -0600

    chore: Renamed shader to have .glsl extension

--- include/core/shaders/vert/main.glsl
+++ include/core/shaders/vert/main.glsl
@@ -1,7 +1,6 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
-layout (location = 1) in vec2 aTexCoord; // the texture coord variable has attribute position 2 
-  
+layout(location = 0) in vec3 aPos; // the position variable has attribute position 0
+layout(location = 1) in vec2 aTexCoord; // the texture coord variable has attribute position 2
 out vec2 TexCoord;
 
 uniform mat4 transform;
@@ -9,9 +8,7 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main()
-{
+void main() {
     gl_Position = projection * view * model * (transform * vec4(aPos, 1.0));
     TexCoord = vec2(aTexCoord.x, aTexCoord.y);
-}   
-
+}
